7e311bc413f2fff7b5cabb2bcd7429fdfcf4c74b
Michael DiGiacomo <mdigiacomo05@me.com>
2021-02-19 10:06:13 -0500

    minor changes to syntax

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,8 @@ var windSpeedEl = document.getElementById("windSpeed");
 
 
 searchButton.addEventListener("click", function() {
-    var cityName = weatherInput.value
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=784e96f1b18fe22b3847fa6ed87bd2bc")
+    var cityName = weatherInput.value;
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=784e96f1b18fe22b3847fa6ed87bd2bc");
     .then(function(response){
         return response.json();
     }).then(function(weather){
@@ -17,13 +17,13 @@ searchButton.addEventListener("click", function() {
         // console.log("Temperature " + weather.main.temp);
         // console.log("humidity " + weather.main.humidity);
         // console.log("wind speed " + weather.wind.speed + "mph");
-        var city = weather.name
-        var temp = weather.main.temp
-        var hum = weather.main.humidity
-        var wind = weather.wind.speed
-        cityNameEl.textContent = city
-        temperatureEl.textContent = temp
-        humidityEl.textContent = hum
-        windSpeedEl.textContent = wind
+        var city = weather.name;
+        var temp = weather.main.temp;
+        var hum = weather.main.humidity;
+        var wind = weather.wind.speed;
+        cityNameEl.textContent = city;
+        temperatureEl.textContent = temp;
+        humidityEl.textContent = hum;
+        windSpeedEl.textContent = wind;
     })
 });
