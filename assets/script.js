035186d41072a6392452f6ae8ecc7956d6ac3517
Michael DiGiacomo <mdigiacomo05@me.com>
2021-02-22 17:50:14 -0500

    added 5 day forcast and converted temp to fariegnheight

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,29 @@ var windSpeedEl = document.getElementById("windSpeed");
 
 searchButton.addEventListener("click", function() {
     var cityName = weatherInput.value;
+    save(cityName);
+    searchweather(cityName);
+    searchFiveDay(cityName);
+});
+
+function save(newCity) {
+    var allCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+    allCities.push(newCity);
+    localStorage.setItem("savedCities", JSON.stringify(allCities));
+}
+
+function searchweather(cityName) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=784e96f1b18fe22b3847fa6ed87bd2bc")
     .then(function(response){
         return response.json();
     }).then(function(weather){
-        // console.log(weather);
-        // console.log(weather.name);
-        // console.log("Temperature " + weather.main.temp);
-        // console.log("humidity " + weather.main.humidity);
-        // console.log("wind speed " + weather.wind.speed + "mph");
+        console.log(weather);
+        console.log(weather.name);
+        console.log("Temperature " + weather.main.temp);
+        console.log("humidity " + weather.main.humidity);
+        console.log("wind speed " + weather.wind.speed + "mph");
         var city = weather.name;
-        var temp = weather.main.temp;
+        var temp = convertKelvin(weather.main.temp);
         var hum = weather.main.humidity;
         var wind = weather.wind.speed;
         cityNameEl.textContent = city;
@@ -26,4 +38,25 @@ searchButton.addEventListener("click", function() {
         humidityEl.textContent = hum;
         windSpeedEl.textContent = wind;
     })
-});
+}
+
+function searchFiveDay(cityName) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=784e96f1b18fe22b3847fa6ed87bd2bc")
+    .then(function(response){
+        return response.json();
+    }).then(function(weather){
+        console.log(weather);
+        var weatherArray = weather.list;
+        for (var i = 0; i < weatherArray.length; i++) {
+            if(weatherArray[i].dt_txt.endsWith("12:00:00")) {
+            console.log(weatherArray[i].dt_txt);
+            console.log(convertKelvin(weatherArray[i].main.temp));
+            console.log(weatherArray[i].main.humidity);
+            }
+        }
+    })
+}
+
+function convertKelvin (kelvin) {
+    return parseInt((kelvin - 273.15) * 1.8 + 32);
+  }
